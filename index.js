3006e5dc7e976d4fb7de13cb78aaf5dd45f27832
Santiago Guerci <santiago.38@hotmail.com>
2024-01-24 14:04:03 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://pokeapi.co/api/v2";
+
+//Home
+app.get("/", (req, res) => {
+  res.render("index.ejs", { title: "Hola Mundo!" });
+});
+
+//Get list of 10 random pokemons
+app.post("/page/:id", async (req, res) => {
+  const id = req.params.id;
+  const response = await axios.get(API_URL + "/pokemon?limit=10"); //esto no esta trayendo random pero trae los primeros 10 pokemones consecutivos
+  const data = response.data.results;
+  console.log(data);
+  res.render("index.ejs", { title: "Lista de Pokemons", data: data });
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
