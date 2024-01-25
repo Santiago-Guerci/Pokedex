cd1f58e6df8692bcddcd6996b5326502cf9c8a03
Santiago-Guerci <santiago.38@hotmail.com>
2024-01-25 00:12:08 -0300

    Fetch 10 pokemons in 3 pages

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.get("/", (req, res) => {
   res.render("index.ejs", { title: "Hola Mundo!" });
 });
 
-//Get list of 10 random pokemons
+//Get list of 10 random pokemons in consecutive order
 app.post("/page/:id", async (req, res) => {
   const id = req.params.id;
-  const response = await axios.get(API_URL + "/pokemon?limit=10"); //esto no esta trayendo random pero trae los primeros 10 pokemones consecutivos
+  const response = await axios.get(API_URL + `/pokemon/?limit=10&offset=${id}`);
   const data = response.data.results;
-  console.log(data);
+  // console.log(data);
   res.render("index.ejs", { title: "Lista de Pokemons", data: data });
 });
 
